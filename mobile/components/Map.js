276f6b0908dde4493bad12d7e26570f4587def63
belen <bucetabmb@gmail.com>
2022-10-30 15:46:47 -0300

    Add focus en el mapa + permiso para localizar ubicacion actual, marker map apunta a current location para simplificar x ahora, en profile se muestra direccion de current location

--- mobile/components/Map.js
+++ mobile/components/Map.js
@@ -1,7 +1,8 @@
-import React, {useContext, useEffect, useRef} from 'react';
+import React, {useContext, useEffect, useRef, useState} from 'react';
 import {StyleSheet, Dimensions} from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import MapView, { Marker} from 'react-native-maps';
 import { FiuberContext } from '../context/FiuberContext';
+import DefaultDestination from './DefaultDestination';
 
 // const {width, height} = Dimensions.get("window");
 
@@ -10,33 +11,62 @@ import { FiuberContext } from '../context/FiuberContext';
 // const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
 const Map = () => {
-
+   
     const {currentDestination} = useContext(FiuberContext);
     const mapRef = useRef(MapView);
+    const [region, setRegion] = useState({
+        latitude: currentDestination.latitude,
+        longitude: currentDestination.longitude,
+        latitudeDelta: 0.03,
+        longitudeDelta: 0.03,
+    })
 
-    const moveTo = async (currentDestination) => {
+   
+   /*  const moveTo = async (currentDestination) => {
+        console.log("current destination ", currentDestination)
         const camera = await mapRef.current?.getCamera();
+        
+        console.log("camera",camera)
+        console.log("current destination ", currentDestination)
         if (camera) {
             console.log("Moving to ", camera.center);
+           
             camera.center = {
                 latitude: currentDestination.latitude,
-                longitude: currentDestination.longitude
+                longitude: currentDestination.longitude,
+                latitudeDelta: 0.09,
+                longitudeDelta:0.04,
+                
+            
             }
-            mapRef.current?.animateCamera(camera, {duration: 1000});
-        }
+            camera.zoomEnabled=true
+            console.log("Moving to ", camera.center);
+            mapRef.current?.animateCamera(camera, {duration: 3000});
+        
+        } 
+  
         return;
-    }
+    } */
 
     useEffect(() => {
-        moveTo(currentDestination);
+
+       // moveTo(currentDestination);
+  
+      
         
     }, []);
 
     return (
         <MapView 
+            region={region}
+            zoomEnabled={true}
+            zoomTapEnabled={true}
+            scrollDuringRotateOrZoomEnabled={true}
             ref={mapRef}
             style={styles.map}
             showsUserLocation={true}
+            onRegionChangeComplete={region => setRegion(region)}
+  
         >
             <Marker coordinate={{latitude: currentDestination.latitude, longitude: currentDestination.longitude}} />
         </MapView>
